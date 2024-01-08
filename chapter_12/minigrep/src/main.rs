c756debfe21a32162cfe72115584afaa41094af9
Eddie Lazar <eddie.lazar@yahoo.com>
2024-01-08 02:02:26 -0500

    making progress on Chapter 12 mini-project

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use minigrep::*;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Error: {e}"); 
+        process::exit(1)
+    };
 }
+
